0d72d9895294d9337f56449d90eaa996f832e988
Rejish007 <stha.rejish07@gmail.com>
2024-10-03 08:11:05 +0545

    handale

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -3,6 +3,8 @@ import {v2 as cloudinary} from 'cloudinary'
 
 const connectCloudinary = async ()=>{
     await cloudinary.config({
-        cloud_name:
+        cloud_name:process.env.CLOUDINARY_NAME,
+        api_key:process.env.CLOUDINARY_API_KEY,
+        
     })
 }
